1d76ad06eaa9c7c545395162e8f3c0a41dd868c4
skyspo <skysport202506@gmail.com>
2026-05-14 23:02:37 +0500

    задание3

---
+++ adress.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, index, city, street, dom, kv):
+        self.index = index
+        self.city = city
+        self.street = street
+        self.dom = dom
+        self.kv = kv
+
---
+++ smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone:
+    def __init__(self, marka, model, number):
+        self.marka = marka
+        self.model = model
+        self.number = number
+
+
